458ac47e42ce1a0c271e0521e39bf08a97e7cd13
Parker <parker.soravee@gmail.com>
2022-04-26 23:14:32 +0700

    update units and tags

--- lib/providers/tags.dart
+++ lib/providers/tags.dart
@@ -11,21 +11,24 @@ class Tag {
 
 class Tags with ChangeNotifier {
   List<Tag> _items = [
-    Tag('1', 'Dairy', AppColors.yellow.hashCode),
-    Tag('2', 'Vegetable', AppColors.lightGreen.hashCode),
-    Tag('3', 'Meat', AppColors.orange.hashCode),
-    Tag('4', 'Fresh', AppColors.green.hashCode),
+    Tag('6267c066c1550ec4cddf83a4', 'dairy', 0xFFFCCA46),
+    Tag('6267c0abc1550ec4cddf83a6', 'Vegetable', AppColors.lightGreen.hashCode),
+    Tag('6267c0c6c1550ec4cddf83a8', 'Meat', 0xFFFE7F2D),
+    Tag('6267c0d6c1550ec4cddf83aa', 'Fresh', AppColors.green.hashCode),
+    Tag('6267c0f4c1550ec4cddf83ac', 'seasoning', 0xFFFCCA46),
+    Tag('6267c11ec1550ec4cddf83ae', 'dessert', 0xFFFE532D),
+    Tag('6267c146c1550ec4cddf83b0', 'sause', 0xFFFE7F2D),
   ];
 
-  List<Tag> _defaultTags = [
-    Tag('1', 'Dairy', AppColors.yellow.hashCode),
-    Tag('2', 'Vegetable', AppColors.lightGreen.hashCode),
-    Tag('3', 'Meat', AppColors.orange.hashCode),
-    Tag('4', 'Fresh', AppColors.green.hashCode),
-  ];
+  // List<Tag> _defaultTags = [
+  //   Tag('1', 'Dairy', AppColors.yellow.hashCode),
+  //   Tag('2', 'Vegetable', AppColors.lightGreen.hashCode),
+  //   Tag('3', 'Meat', AppColors.orange.hashCode),
+  //   Tag('4', 'Fresh', AppColors.green.hashCode),
+  // ];
 
   List<Tag> get defaultTags {
-    return [..._defaultTags];
+    return [..._items];
   }
 
   List<Tag> getTagsById(List<String> tagIds) {
--- lib/providers/unit.dart
+++ lib/providers/unit.dart
@@ -1,25 +1,69 @@
 import 'package:flutter/material.dart';
 
 class Unit {
-  List<String> _items = [
-    'Pieces',
-    'Grams',
-    'Kilograms',
-    'Litre',
-    'Millilitre',
-    'Ounce',
-    'CC',
+  String id;
+  String name;
+
+  Unit({required this.id, required this.name});
+}
+
+class Units {
+  List<Unit> _items = [
+    Unit(
+      id: '62681408c5b8172fe1b14476',
+      name: 'Pieces',
+    ),
+    Unit(
+      id: '626814a9c5b8172fe1b1448a',
+      name: 'Grams',
+    ),
+    Unit(
+      id: '62681434c5b8172fe1b1447c',
+      name: 'Milligrams',
+    ),
+    Unit(
+      id: '6268143ac5b8172fe1b1447e',
+      name: 'Kilograms',
+    ),
+    Unit(
+      id: '62681447c5b8172fe1b14480',
+      name: 'Litre',
+    ),
+    Unit(
+      id: '6268144dc5b8172fe1b14482',
+      name: 'Millilitre',
+    ),
+    Unit(
+      id: '62681458c5b8172fe1b14484',
+      name: 'Ounce',
+    ),
+    Unit(
+      id: '626814a3c5b8172fe1b14488',
+      name: 'CC',
+    ),
   ];
 
-  Unit();
+  Units();
 
-  List<String> get items {
+  List<Unit> get items {
     return [..._items];
+  }
+
+  List<String> get itemsString {
+    return _items.map((e) => e.name).toList();
+  }
+
+  String getNameById(String id) {
+    return _items.firstWhere((element) => element.id == id).name;
+  }
+
+  String getIdByName(String name) {
+    return _items.firstWhere((element) => element.name == name).id;
   }
 
   List<DropdownMenuItem<String>> getDropdownMenuItem(
       double fontSize, Color color) {
-    return [...items].map<DropdownMenuItem<String>>((String value) {
+    return itemsString.map<DropdownMenuItem<String>>((String value) {
       return DropdownMenuItem<String>(
         value: value,
         child: Text(
